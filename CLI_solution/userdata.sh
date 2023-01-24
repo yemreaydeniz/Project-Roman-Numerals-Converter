26567bd6d425bd36b4925d6efba68f941e2c6ab5
yemre <yemreaydeniz1@gmail.com>
2023-01-24 09:14:01 +0100

    userdata.sh and .yml files are modified.

--- CLI_solution/userdata.sh
+++ CLI_solution/userdata.sh
@@ -4,7 +4,7 @@ yum install python3 -y
 pip3 install flask
 yum install git -y
 cd /home/ec2-user
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/index.html
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/result.html
-wget https://raw.githubusercontent.com/serdarcw/cli_deneme/master/app.py
+wget -P templates https://raw.githubusercontent.com/yemreaydeniz/Project-Roman-Numerals-Converter/main/templates/index.html
+wget -P templates https://raw.githubusercontent.com/yemreaydeniz/Project-Roman-Numerals-Converter/main/templates/result.html
+wget https://raw.githubusercontent.com/yemreaydeniz/Project-Roman-Numerals-Converter/main/project-explained.py
 python3 app.py
